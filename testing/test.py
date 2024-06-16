4f25801247edaf72c616c2c81870058b19d533e2
ikeda042 <ikeda042main@gmail.com>
2024-06-17 02:15:37 +0900

    TC

--- testing/test.py
+++ testing/test.py
@@ -6,20 +6,22 @@ import matplotlib.pyplot as plt
 def get_contour(image_path: str) -> np.ndarray:
     image = cv2.imread(image_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+    ret, thresh = cv2.threshold(gray, 130, 255, cv2.THRESH_BINARY)
     img_canny = cv2.Canny(thresh, 0, 150)
     contours_raw, hierarchy = cv2.findContours(
         img_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
     )
     # # Filter out contours with small area
-    # contours = list(filter(lambda x: cv2.contourArea(x) >= 300, contours_raw))
-    return contours_raw
+    contours = list(filter(lambda x: cv2.contourArea(x) >= 300, contours_raw))
+    return contours
 
 
 def plot_contour(contour: np.ndarray):
     plt.figure()
     for i in range(len(contour)):
         plt.scatter(contour[i][:, 0, 0], contour[i][:, 0, 1], s=1)
+    plt.gca().set_aspect("equal")
+    plt.tick_params(axis="both", which="both", direction="in")
     plt.savefig("testing/contour.png", dpi=300)
     plt.close()
 
