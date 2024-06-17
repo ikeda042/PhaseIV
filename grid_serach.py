35c24cb27c01c8d15ffdf81c2485a70b0a153aaf
ikeda042 <ikeda042main@gmail.com>
2024-06-18 03:00:06 +0900

    T

--- grid_serach.py
+++ grid_serach.py
@@ -8,9 +8,6 @@ from typing import Annotated
 from pydantic.fields import Field
 import os
 import shutil
-from concurrent.futures import ThreadPoolExecutor, as_completed
-import queue
-import threading
 
 CannyParamInt = Annotated[int, Field(gt=1, lt=254)]
 
@@ -27,7 +24,7 @@ def get_contour(image, canny_param_lower: CannyParamInt, canny_param_upper: Cann
 def process_image(image, lower: CannyParamInt, upper: CannyParamInt):
     contours = get_contour(image, lower, upper)
     contour_sum = sum([cv2.contourArea(c) for c in contours])
-    return lower, upper, contour_sum
+    return lower, upper, contours, contour_sum
 
 def plot_heatmap(contour_sums, lower, upper, max_lower, max_upper):
     plt.figure(figsize=(10, 8))
@@ -37,14 +34,6 @@ def plot_heatmap(contour_sums, lower, upper, max_lower, max_upper):
     plt.title(f'Canny Thresholds: Lower={lower}, Upper={upper}')
     plt.savefig(f"tmp/heatmap_{lower}_{upper}.png")
     plt.close()
-
-def plot_worker():
-    while True:
-        task = plot_queue.get()
-        if task is None:
-            break
-        plot_heatmap(*task)
-        plot_queue.task_done()
 
 ############################################################################################################
 image_path = "sample_images/cells_100x_large_scope.png"
@@ -59,20 +48,11 @@ image = cv2.imread(image_path)
 max_threshold = 255
 contour_sums = np.zeros((max_threshold + 1, max_threshold + 1))
 
-plot_queue = queue.Queue()
-plot_thread = threading.Thread(target=plot_worker)
-plot_thread.start()
-
-with ThreadPoolExecutor(max_workers=8) as executor:
-    futures = [executor.submit(process_image, image, lower, upper) for lower in range(1, max_threshold) for upper in range(lower + 1, max_threshold + 1)]
-    
-    for future in tqdm(as_completed(futures), total=len(futures)):
-        lower, upper, contour_sum = future.result()
+for lower in tqdm(range(1, max_threshold)):
+    for upper in range(lower + 1, max_threshold + 1):
+        _, _, _, contour_sum = process_image(image, lower, upper)
         contour_sums[lower, upper] = contour_sum
-        plot_queue.put((contour_sums, lower, upper, max_threshold, max_threshold))
-
-plot_queue.put(None)
-plot_thread.join()
+        plot_heatmap(contour_sums, lower, upper, max_threshold, max_threshold)
 
 images = [imageio.imread(f"tmp/heatmap_{lower}_{upper}.png") for lower in range(1, max_threshold) for upper in range(lower + 1, max_threshold + 1)]
 imageio.mimsave("result_heatmap.gif", images, loop=0)
