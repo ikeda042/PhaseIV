cbc98ef5747be2476741f578d5b1d2ed15b0b151
ikeda042 <ikeda042main@gmail.com>
2024-06-18 02:25:39 +0900

    shut

--- canny_params_testfield.py
+++ canny_params_testfield.py
@@ -5,6 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from typing import Annotated
 from pydantic.fields import Field
+import os
+import shutil
+
 
 CannyParamInt = Annotated[int, Field(gt=1, lt=254)]
 
@@ -41,3 +44,33 @@ def process_image(image, canny_param_int: CannyParamInt):
     contours = get_contour(image, canny_param_int)
     contour_sum = sum([cv2.contourArea(c) for c in contours])
     return canny_param_int, contours, contour_sum
+
+
+def plot_contour_sum(global_contour_sums, i):
+    plt.figure()
+    plt.plot(range(1, i + 1), global_contour_sums[:i], marker="o")
+    plt.xlabel("Canny Threshold")
+    plt.ylabel("Area")
+    plt.xlim(0, 254)
+    # meta params
+    plt.ylim(0, 160000)
+    plt.savefig(f"tmp/contour_sum_{i}.png", dpi=300)
+    plt.close()
+
+
+############################################################################################################
+image_path = "testing/test2.png"
+
+############################################################################################################
+try:
+    os.mkdir("tmp")
+except FileExistsError:
+    shutil.rmtree("tmp")
+image = cv2.imread(image_path)
+global_contour_sums = []
+for canny_param_int, contours, contour_sum in tqdm(
+    [process_image(image, i) for i in range(1, 255)]
+):
+    global_contour_sums.append(contour_sum)
+    plot_contour(contours, f"tmp/contour_{canny_param_int}.png", canny_param_int)
+    plot_contour_sum(global_contour_sums, canny_param_int)
