c53085b8917490fdbf5f84278b55b64302a6214c
ikeda042 <ikeda042main@gmail.com>
2024-06-17 14:13:00 +0900

    Test

--- testing/test.py
+++ testing/test.py
@@ -10,7 +10,7 @@ CannyParamInt = Annotated[int, Field(gt=1, lt=254)]
 def get_contour(image_path: str, canny_param_int: CannyParamInt) -> np.ndarray:
     image = cv2.imread(image_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray, 130, 255, cv2.THRESH_BINARY)
+    ret, thresh = cv2.threshold(gray, canny_param_int, 255, cv2.THRESH_BINARY)
     img_canny = cv2.Canny(thresh, 0, 150)
     contours_raw, hierarchy = cv2.findContours(
         img_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
