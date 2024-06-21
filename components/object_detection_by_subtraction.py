4c02de2cb6bfd369017dd0e98b4760b18b6fed6e
ikeda042 <ikeda042main@gmail.com>
2024-06-21 15:37:10 +0900

    cross

--- components/object_detection_by_subtraction.py
+++ components/object_detection_by_subtraction.py
@@ -6,7 +6,6 @@ fgbg = cv2.createBackgroundSubtractorMOG2()
 
 num_images = len(os.listdir("sample_images/sample_tiff"))
 frames = []
-
 for i in range(num_images):
     frame = cv2.imread(f"sample_images/sample_tiff/{i}.tif")
     if frame is None:
@@ -24,9 +23,27 @@ for i in range(num_images):
     contours, _ = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
-        if cv2.contourArea(contour) > 1:
+        if cv2.contourArea(contour) > 200:
             x, y, w, h = cv2.boundingRect(contour)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            center_x = x + w // 2
+            center_y = y + h // 2
+
+            cv2.line(
+                frame,
+                (center_x - 10, center_y),
+                (center_x + 10, center_y),
+                (0, 0, 255),
+                2,
+            )
+            cv2.line(
+                frame,
+                (center_x, center_y - 10),
+                (center_x, center_y + 10),
+                (0, 0, 255),
+                2,
+            )
 
     cv2.putText(
         frame, f"Frame: {i}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2
@@ -37,6 +54,5 @@ for i in range(num_images):
     combined_rgb = cv2.cvtColor(combined, cv2.COLOR_BGR2RGB)
 
     frames.append(combined_rgb)
-
 
 imageio.mimsave("timelapse_sub.gif", frames, duration=0.1, loop=0)
